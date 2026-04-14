f2f0fed0559adfe1cda6d382d755f38860d03b73
Sanju <sanjumalik2006@gmail.com>
2026-04-14 07:09:13 +0530

    Time: 10 ms (83.78%) | Memory: 46.9 MB (61.11%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,35 +1,67 @@
+
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        List<Integer> result=new ArrayList<>();
-        if(s.length()<p.length())
+        List<Integer> result = new ArrayList<>();
+
+        int n = s.length();
+        int m = p.length();
+
+        if (n < m) return result;
+
+        int[] pFreq = new int[26];
+        int[] sFreq = new int[26];
+
+        // Step 1: build p frequency
+        for (int i = 0; i < m; i++) {
+            pFreq[p.charAt(i) - 'a']++;
+        }
+
+        int match = 0;
+
+        // Step 2: first window
+        for (int i = 0; i < m; i++) {
+            sFreq[s.charAt(i) - 'a']++;
+        }
+
+        // Step 3: count initial matches
+        for (int i = 0; i < 26; i++) {
+            if (pFreq[i] == sFreq[i]) {
+                match++;
+            }
+        }
+
+        // Step 4: sliding window
+        for (int i = m; i < n; i++) {
+
+            if (match == 26) {
+                result.add(i - m);
+            }
+
+            int newChar = s.charAt(i) - 'a';
+            int oldChar = s.charAt(i - m) - 'a';
+
+            // add new char
+            sFreq[newChar]++;
+            if (sFreq[newChar] == pFreq[newChar]) {
+                match++;
+            } else if (sFreq[newChar] == pFreq[newChar] + 1) {
+                match--;
+            }
+
+            // remove old char
+            sFreq[oldChar]--;
+            if (sFreq[oldChar] == pFreq[oldChar]) {
+                match++;
+            } else if (sFreq[oldChar] == pFreq[oldChar] - 1) {
+                match--;
+            }
+        }
+
+        // check last window
+        if (match == 26) {
+            result.add(n - m);
+        }
+
         return result;
-        int pCount[]=new int[26];
-        int sCount[]=new int[26];
-        for(char c:p.toCharArray())
-        {
-            pCount[c-'a']++;
-        }
-        int window=p.length();
-        for(int i=0;i<window;i++)
-        {
-            sCount[s.charAt(i)-'a']++;
-        }
-        if(Arrays.equals(pCount,sCount))
-        {
-            result.add(0);
-        }
-        for(int i=window;i<s.length();i++)
-        {
-            char newChar=s.charAt(i);
-             char oldChar=s.charAt(i-window);
-             sCount[newChar-'a']++;
-             sCount[oldChar-'a']--;
-               if(Arrays.equals(pCount,sCount))
-               {
-                 result.add(i-window+1);
-              }
-        }
-        return result;
-       
     }
 }
